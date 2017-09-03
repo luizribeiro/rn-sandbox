f27ace69bac962bcc84b5d74f768eec4f6cc63c4
Luiz Ribeiro <luizribeiro@gmail.com>
2017-09-03 11:51:53 -0400

    Now using Header from react-native-elements

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import React from 'react';
-import { ApolloProvider } from 'react-apollo';
-import { View } from 'react-native';
 import Status from './Status';
+import { ApolloProvider } from 'react-apollo';
+import { Header } from 'react-native-elements';
+import { ScrollView, View, Text } from 'react-native';
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
@@ -15,10 +16,17 @@ const client = new ApolloClient({
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <ApolloProvider client={client}>
-          <Status />
-        </ApolloProvider>
+      <View style={{ flex: 1 }}>
+        <Header
+          statusBarProps={{ barStyle: 'light-content' }}
+          centerComponent={{ text: 'HOME STATUS', style: { color: '#fff'} }} 
+          outerContainerStyles={{ backgroundColor: '#3D6DCC' }}
+        />
+        <ScrollView style={{ marginTop: 70 }}>
+          <ApolloProvider client={client}>
+            <Status />
+          </ApolloProvider>
+        </ScrollView>
       </View>
     );
   }
