f67c7d8b7f0b59a4b1d6fd059fd05d5400fb0f45
Luiz Ribeiro <luizribeiro@gmail.com>
2017-10-01 23:42:21 -0300

    Added progress circle and some smaller cosmetic changes

--- Status.js
+++ Status.js
@@ -2,10 +2,11 @@
  * @flow
  */
 
+import * as Progress from "react-native-progress";
 import React from "react";
 import gql from "graphql-tag";
 import { AppLoading } from "expo";
-import { Card } from "react-native-elements";
+import { Button, Card } from "react-native-elements";
 import { View, Text } from "react-native";
 import { graphql } from "react-apollo";
 
@@ -33,13 +34,59 @@ export const Status = ({ data: { loading, thermostat, vacuum } }: Props) => {
 
   return (
     <View>
+      <Card title="Vacuum">
+        <View style={{ flex: 1, flexDirection: "row", marginBottom: 20 }}>
+          <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <Text style={{ fontWeight: "bold", fontSize: 22 }}>
+              {vacuum.state.charAt(0).toUpperCase() +
+                vacuum.state.slice(1).toLowerCase()}
+            </Text>
+          </View>
+          <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <Text style={{ fontWeight: "bold", marginBottom: 8 }}>Battery</Text>
+            <Progress.Circle
+              size={80}
+              showsText={true}
+              progress={vacuum.battery / 100.0}
+              formatText={progress => `${vacuum.battery}%`}
+              thickness={5}
+              color="#427AA1"
+            />
+          </View>
+        </View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          {vacuum.state === "CLEANING" ? (
+            <Button
+              icon={{ name: "pause" }}
+              title="Pause"
+              containerViewStyle={{ flex: 1, marginLeft: 0, marginRight: 0 }}
+              backgroundColor="#427AA1"
+            />
+          ) : (
+            <Button
+              icon={{ name: "play-arrow" }}
+              title="Clean"
+              containerViewStyle={{ flex: 1, marginLeft: 0, marginRight: 0 }}
+              backgroundColor="#679436"
+            />
+          )}
+          <View style={{ width: 2 }} />
+          <Button
+            disabled={vacuum.state !== "PAUSED"}
+            icon={{ name: "home" }}
+            title="Dock"
+            containerViewStyle={{ flex: 1, marginLeft: 0, marginRight: 0 }}
+            backgroundColor="#427AA1"
+          />
+        </View>
+      </Card>
       <Card title="Thermostat">
         <Text>The thermostat is {thermostat.mode}.</Text>
         <Text>Current temperature is {thermostat.currentTemperature}C.</Text>
-      </Card>
-      <Card title="Vacuum">
-        <Text>The vacuum is {vacuum.state}.</Text>
-        <Text>The vacuum's battery is at {vacuum.battery}%.</Text>
       </Card>
     </View>
   );
---
+++ flow-typed/react-native-progress.js
@@ -0,0 +1,3 @@
+declare module 'react-native-progress' {
+  declare module.exports: any;
+}
